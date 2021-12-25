075b01f63ee22de1f3a3a5fda6559ec1bee84b09
abaha91 <abashkin.91@Mail.ru>
2021-12-25 17:49:48 +0300

    [TEST COMMIT] added console.log(port)

--- server/server.js
+++ server/server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 const path = require('path');
-const port = 80;
+const port = process.env.PORT || 80;
 
 app.use(express.static(path.join(__dirname, '../', 'build')));
 
@@ -9,4 +9,6 @@ app.get('/*', function (req, res) {
     res.sendFile(path.join(__dirname, '../', 'build', 'index.html'));
 });
 
-app.listen(port);
+app.listen(port, function(){
+    console.log('Listening on port ' + listener.address().port); //Listening on port 8888
+});
